e4a637d1c540240e621489aa1b53ef5fb96de6c8
cenpnt <139846503+cenpnt@users.noreply.github.com>
2023-09-15 22:28:20 +0700

    2_1.rs

--- hw7/src/bin/2_1.rs
+++ hw7/src/bin/2_1.rs
@@ -1,35 +1,33 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    let mut result: Vec<(f64,f64)> = Vec::new();
-
-    let mut new_args = args.len();
-    if args.len() % 2 != 0{
-        new_args -= 1;
+    if args.len() < 2 {
+        return;
     }
 
-    for i in 1..new_args {
-        if args[i].parse::<f64>().is_ok() == false{
-            return println!("Invalid input")
+    let mut check: Vec<f64> = Vec::new();
+
+    for i in 1..args.len() {
+        if args[i].parse::<f64>().is_ok() == false {
+            println!("Invalid input");
+            return;
         }
-        if i % 2 == 0{
-            continue;
-        }else{   
-            if i+1 >= new_args{           
-            break;
-        }
-        result.push((args[i].parse().unwrap(), args[i+1].parse().unwrap()));
-        }  
-    }  
-    result.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap()); 
-    println!("Ascending order by x: {:?}", result);
+        check.push(args[i].parse().unwrap());
+    }
 
-    result.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
-    println!("Descending order by x: {:?}", result);
+    if check.len() % 2 != 0 {       
+        check.pop();
+    }
 
-    result.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap()); 
-    println!("Ascending order by y: {:?}", result);
+    let mut result: Vec<(f64, f64)> = Vec::new();
 
-    result.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
-    println!("Descending order by y: {:?}", result);
+    for j in 0..check.len() / 2 {
+        result.push((check[j * 2], check[j * 2 + 1]));
+    }
+
+    result.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap().then_with(|| a.1.partial_cmp(&b.1).unwrap()));
+    println!("Ascending order: {:?}", result);
+
+    result.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap().then_with(|| b.1.partial_cmp(&a.1).unwrap()));
+    println!("Descending order: {:?}", result);
 }
